7ed58fdfd86c9979372126ffe67c31c1d9b9314a
unknown <mariacecilia@edu.unifil.br>
2024-07-16 15:40:10 -0300

    feat: adicionando secao sobremim

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SobreMimComponent } from '../sobre-mim/sobre-mim.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SobreMimComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
